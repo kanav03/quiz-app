71a08f59d5fe5e0f23bbecbad8f2205bdb011ff7
Kanav Nijhawan <nijhawankanav@gmail.com>
2023-10-29 19:10:52 +0530

    deploy

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/asnwer_button.dart';
+import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions_data.dart';
 import 'package:google_fonts/google_fonts.dart';
 
